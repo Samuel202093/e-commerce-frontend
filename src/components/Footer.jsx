a3f2f84478eb140d70748d1110238f480b1867c0
Samuel202093 <samuelonyebueke0311@gmail.com>
2023-05-24 14:21:51 +0100

    updated ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React,{ useState} from "react";
 import "../assets/css/newsCard.css";
 import { FaFacebookF } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
+import axios from 'axios'
+import { url } from '../datas/api'
+import { toast } from "react-toastify"
+import ClipLoader from 'react-spinners/ClipLoader'
 import {Link} from 'react-router-dom'
 
+const override= {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "white",
+}
+
 const Footer = () => {
+  const [email, setEmail] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [color, setColor] = useState('#ffff')
+
+
+  const handleChange = (e)=>{
+    setEmail(e.target.value)
+  }
+
+  const handleSubscribe = async(e)=>{
+    try {
+      e.preventDefault()
+      setLoading(true)
+      await axios({
+        method: "post",
+        url:`${url}/customer/subscribe`,
+        data: email,
+  
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }).then((res)=>{
+        if (res.status === 200) {
+          toast.success("Thanks for subscribing.")
+          setLoading(false)
+        }
+      })
+    } catch (error) {
+      toast.error('Network error. Please try again!!!')
+    }
+  
+  }
   return (
     <footer>
       <div className="gadget-wrapper">
@@ -38,8 +80,17 @@ const Footer = () => {
       <div className="subscribe-wrapper">
         <h4>Subscribe To Our Email</h4>
         <h1>For Latest News & Updates</h1>
-        <input type='text' placeholder= 'Enter Your Email' />
-        <button type="submit">Subscribe</button>
+        <input type='text' placeholder= 'Enter Your Email' name="email" onChange={handleChange} value={email}/>
+        <button type='submit' onClick={handleSubscribe}>{loading === true ? (<ClipLoader
+                         color={color}
+                         loading={loading}
+                         cssOverride={override}
+                         size={25}
+                         speedMultiplier={0.7}
+                         aria-label="Loading Spinner"
+                         data-testid="loader"
+                         />):'Subscribe'}</button>
+        {/* <button type="submit" onClick={handleSubscribe}>Subscribe</button> */}
       </div>
     </footer>
   );
